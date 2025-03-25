e14fb479d90266a40aa36af70ce2fba120bd3298
Zaahiid <zahidrsheikh01@gmail.com>
2025-03-25 18:15:04 +0530

    Added Board, Comment, Follow and Like model

---
+++ src/model/follow.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const followSchema = new mongoose.Schema(
+  {
+    follower: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    following: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Follow", followSchema);
